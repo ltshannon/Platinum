7d415ba668d2fd9740832b174f311faf5bd7a529
Larry Shannon <larry@breakawaydesign.com>
2024-06-12 15:45:32 -0700

    Initial Commit

---
+++ Platinum/PlatinumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatinumApp.swift
+//  Platinum
+//
+//  Created by Larry Shannon on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PlatinumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
